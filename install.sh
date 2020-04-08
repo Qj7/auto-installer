00a2bef96ee8880739b77990c78daacccbcdc86e
Vladimir Dolgiy <vladimir.dolgiy@apliteni.com>
2020-04-08 03:33:26 +0300

    fix ruby for user

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+RESET_FORMATTING='\e[0m'
+COLOR_CODE['red']=31
+
 is_root?
 curl -fsSL https://github.com/Qj7/auto-installer/archive/master.tar.gz | tar xz
 cd auto-installer-master
@@ -7,10 +10,21 @@ ansible-playbook playbook.yml -vvv
 cd ..
 rm -rf auto-installer-master
 
-is_root?() {
-    if
-      [[ "${USER:-}" == "root" ]] ; then
-      echo "This script works only with normal user, it wont work with root, please log in as normal user and try again."
-      exit 1
-    fi
+is_root?(){
+  if [[ "${USER:-}" == "root" ]] ; then
+    local message="This script works only with normal user, it wont work with root, please log in as normal user and try again."
+    print_with_color ${message} 'red'
+    exit 1
+  fi
+}
+
+print_with_color(){
+  local message="${1}"
+  local color="${2}"
+  if ! empty "$color"; then
+    escape_sequence="\e[${COLOR_CODE[$color]}m"
+    echo -e "${escape_sequence}${message}${RESET_FORMATTING}"
+  else
+    echo "$message"
+  fi
 }
